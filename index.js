5bd6c1d3adef3018299e1e2d73617228c608d6a0
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-02-15 11:45:58 -0500

    commit tree 4bd45d44935215cd4eae3c4ef1f40a9a12c62c75

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+
+class Polygon {
+    constructor(arr) {
+        this.arr = arr;
+    }
+
+    get countSides() {
+        return this.arr.length
+    }
+
+    get perimeter() {
+        return this.arr.reduce(
+            (previousValue, currentValue) => previousValue + currentValue
+        )
+    }
+} 
+
+class Triangle extends Polygon {
+    get isValid() {
+        if (!Array.isArray(this.arr)) return;
+        if (this.count !== 3) return;
+        let side1 = this.arr[0]
+        let side2 = this.arr[1]
+        let side3 = this.arr[2]
+        return ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1))
+      }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        //
+    };
+
+    get area() {
+        return this.arr[0] * this.arr[0]
+    }
+}
